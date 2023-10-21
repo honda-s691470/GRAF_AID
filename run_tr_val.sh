eccfe63a5f9e8b8b40fb04dacebf8543e04d5b2d
Suguru Honda <80377824+honda-s691470@users.noreply.github.com>
2023-10-21 16:30:00 +0900

    Update run_tr_val.sh

--- run_tr_val.sh
+++ run_tr_val.sh
@@ -1,8 +1,4 @@
 #!/bin/bash
-#SBATCH -p G
-#SBATCH -J sq
-#SBATCH -o stdout.%J
-#SBATCH -e stderr.%J
 
 if [ "$#" -lt 6 ]; then
     echo "Usage: $0 [data_folder] [train_val_data] [p_threshold] [fold_num] [analysis_date] [MHC] [num_test_model] [Select_file]"
@@ -18,18 +14,11 @@ MHC="${6:-Only}"
 num_test_model="${7:-3}"
 Select_file="${8:-GCN_MAT_snp_list_r2_02.snp}"
 
-echo "p_threshold original: $p_threshold"
 
 IFS=' ' read -ra p_threshold_array <<< "$p_threshold"
 for p in "${p_threshold_array[@]}"; do
     p_threshold_args+=" $p"
 done
 
-echo "p_threshold args: $p_threshold_args"
-
-
 python split.py --data_folder ${data_folder} --train_val_data ${train_val_data} --p_threshold ${p_threshold_args} --fold_num ${fold_num}
 python run_tr_val.py --data_folder ${data_folder} --p_threshold ${p_threshold_args} --fold_num ${fold_num} --MHC ${MHC} --Select_file ${Select_file} --num_test_model ${num_test_model} --analysis_date_for_test ${analysis_date}
-python make_statsum.py --data_folder ${data_folder} --p_threshold ${p_threshold_args} --fold_num ${fold_num} --MHC ${MHC} --Select_file ${Select_file} --num_test_model ${num_test_model} --analysis_date_for_test ${analysis_date}
-
-#nohup sh run_tr_val.sh > output_RA_WB_r2_1.log 2>&1 &
