2476e447b75f57763af016a326ca4bd0d0b25110
Suguru Honda <80377824+honda-s691470@users.noreply.github.com>
2023-11-10 16:59:37 +0900

    Add files via upload

--- make_mat_for_gcn.sh
+++ make_mat_for_gcn.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 if [ "$#" -lt 4 ]; then
-    echo "Usage: $0 <gwas> <snp_list> <bfile> <pheno_file> [k] [p_val] [population] [disease] [r2]"
+    echo "Usage: $0 <gwas> <snp_list> <bfile> <pheno_file> [k] [r2] [p_val] [population] [disease]"
     exit 1
 fi
 
@@ -11,9 +11,9 @@ bfile=$3
 pheno_file=$4
 k="${5:-5}"
 p_val="${6:-0.01}"
-population="${7:-TEST}"
-disease="${8:-DIS1}"
-r2="${9:-2}"
+r2="${7:-2}"
+population="${8:-TEST}"
+disease="${9:-DIS1}"
 
 mkdir SNP_select
 make_snp_list() {
@@ -21,6 +21,7 @@ make_snp_list() {
     c_r2=$(echo "0.1 * $r2" | bc)
     plink --bfile ${bfile} --clump ${gwas} --clump-r2 ${c_r2} --clump-kb 250 --clump-p1 ${p_val}  --out GCN_MAT_snp_list_r2_0${r2}
     awk 'NR > 1 {print $3}' GCN_MAT_snp_list_r2_0${r2}.clumped > SNP_select/GCN_MAT_snp_list_r2_0${r2}.snp
+    snp_list="SNP_select/GCN_MAT_snp_list_r2_0${r2}.snp"
 }
 
 process_gwas_data() {
@@ -37,9 +38,9 @@ process_gwas_data() {
             }
         }
     }
-    NR>1 { print $1, $col_index }' ${gwas} | sort > GWAS_SNP_P
+    NR>1 { print $1, $col_index }' ${gwas} | sort -k1,1 -u | uniq > GWAS_SNP_P
 
-    sort SNP_select/${snp_list} | join - <(awk -v p=${p_val} '($2 <= p){print $1, $2}' GWAS_SNP_P) > merge_01
+    sort SNP_select/${snp_list} | join -1 1 -2 1 - <(awk -v p=${p_val} '($2 <= p){print $1, $2}' GWAS_SNP_P) | sort -u > merge_01
     echo "finish process_gwas_data"
     rm GWAS_SNP_P
 }
@@ -123,13 +124,14 @@ perform_kfold_cv() {
         awk "NR < $start || NR > $end" "$shuffled_file" >> "${output_dir}/cv/cross_train_${i}"
     done
 
-    rm $shuffled_file  # cleanup
+    rm $shuffled_file
 }
 
 for popu in ${population}; do
     if [ "$snp_list" = "nan" ] || [ "$snp_list" = "Nan" ]; then
         make_snp_list
     fi
+    echo $snp_list
     process_gwas_data $popu
     generate_matrices $popu
     set_header
